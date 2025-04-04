623d566979e42c2a12daca3db4da62bc262da397
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2025-04-04 04:08:11 -0400

    feat: use dark tilelayer

--- src/components/map/map-components.tsx
+++ src/components/map/map-components.tsx
@@ -2,6 +2,8 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./map.css";
 
 const icon = L.icon({
   iconUrl: "/marker-icon.png",
@@ -49,12 +51,15 @@ export default function MapComponents() {
     <MapContainer
       center={[center[0], center[1]]}
       zoom={16}
-      scrollWheelZoom={false}
+      scrollWheelZoom={true}
       style={{ height: "100%", width: "100%", borderRadius: "0.5rem" }}
+      className="z-0"
+      zoomControl={false}
+      attributionControl={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution=""
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
       />
 
       {incidents.map((incident) => (
